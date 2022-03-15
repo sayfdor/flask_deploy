4e6e48382fb4c505ec587b0e0e894bda1c4a0234
Misha <mrdesper.mi@gmail.com>
2022-03-15 17:47:21 +0500

    commit 3 files

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Привет от приложения Flask"
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
